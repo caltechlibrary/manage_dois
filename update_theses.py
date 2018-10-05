81b56bb2e29aa46bd6f57f1f14e85b5605e1ad62
Tom Morrell <tmorrell@caltech.edu>
2018-10-05 16:41:48 -0700

    Working update

--- update_theses.py
+++ update_theses.py
@@ -1,7 +1,32 @@
-import requests, sys, csv
+import requests, sys, csv, getpass,subprocess
+import xmltodict
+import caltech_thesis
+from datacite import schema40,DataCiteMDSClient
 
-def update_record():
-    print('hello')
+def update_record(idv,username,password,datacite_password):
+    url = 'https://'+username+':'+password+'@thesis.library.caltech.edu/rest/eprint/'
+    record_url = url + str(idv) +'.xml'
+    record = subprocess.check_output(["eputil",record_url],universal_newlines=True)
+    eprint = xmltodict.parse(record)['eprints']['eprint'] 
+    metadata = caltech_thesis.epxml_to_datacite(eprint)
+    
+    assert schema40.validate(metadata)
+    #Debugging if this fails
+    #v = schema40.validator.validate(metadata)
+    #errors = sorted(v.iter_errors(instance), key=lambda e: e.path)
+    #for error in errors:
+    #        print(error.message)
+
+    # Initialize the MDS client.
+    d = DataCiteMDSClient(
+    username='CALTECH.LIBRARY',
+    password=datacite_password,
+    prefix='10.7907',
+    )
+
+    xml = schema40.tostring(metadata)
+    result = d.metadata_post(xml)
+    print(result)
 
 def validate_response(response):
     if (response.status_code != 200):
@@ -9,6 +34,17 @@ def validate_response(response):
         exit()
     else:
         return response.json()
+
+def trace_url(url):
+    get = requests.get(url)
+    return get.url
+
+username = input('Enter your username: ')
+password = getpass.getpass()
+
+#Get our DataCite password
+infile = open('pw','r')
+datacite_password = infile.readline().strip()
 
 endpoint = 'https://api.datacite.org/works'
 query = endpoint + '?query=10.7907&data-center-id=caltech.library'
@@ -20,24 +56,27 @@ records = response['data']
 output = open('records.tsv','w')
 writer = csv.writer(output, delimiter='\t')
 writer.writerow(\
-        ['Eprints ID','DOI','Degree Date','Author Name','Title','PURL'])
+        ['Eprints ID','DOI','Degree Date','Author Name','Title','URL'])
 page = 1
 count = 0
 while page <= pages:
     for r in records:
-        if r['attributes']['resource-type-subtype'] != 'Dissertation':
-            author = r['attributes']['author'][0]
-            if 'family' in author:
-                name = author['family']+', '+author['given']
-            elif 'literal' in author:
-                name = author['literal']
-            else:
-                print(r['attributes']['author'][0])
-                exit()
-            print(r['attributes']['doi'])
-            writer.writerow(['',r['attributes']['doi'],\
-            r['attributes']['published'],name,r['attributes']['title'],\
-            r['attributes']['url']])
+        author = r['attributes']['author'][0]
+        if 'family' in author:
+            name = author['family']+', '+author['given']
+        elif 'literal' in author:
+            name = author['literal']
+        else:
+            print(r['attributes']['author'][0])
+            exit()
+        doi = r['attributes']['identifier']
+        print(doi)
+        url = trace_url(doi)
+        split = url.split('/')
+        if split[2]=='thesis.library.caltech.edu':
+                update_record(split[3],username,password,datacite_password)
+                writer.writerow(['',r['attributes']['doi'],\
+                r['attributes']['published'],name,r['attributes']['title'],url])
         count = count + 1
     page = page + 1
     response = requests.get(query + '&page[number]='+str(page))
